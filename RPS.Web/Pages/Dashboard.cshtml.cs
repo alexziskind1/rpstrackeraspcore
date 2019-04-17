5f4a47a951d3d620976ca073f3abfe3ff13bd5a5
alexziskind1 <alex@nuvious.com>
2019-04-17 16:05:41 -0700

    dashboard done

--- RPS.Web/Pages/Dashboard.cshtml.cs
+++ RPS.Web/Pages/Dashboard.cshtml.cs
@@ -4,14 +4,54 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RPS.Core.Models.Dto;
+using RPS.Data;
 
 namespace RPS.Web.Pages
 {
     public class DashboardModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly IPtDashboardRepository rpsDashRepo;
 
+        public DateTime? DateStart { get; set; }
+        public DateTime? DateEnd { get; set; }
+
+        public int IssueCountOpen { get; set; }
+        public int IssueCountClosed { get; set; }
+
+        public int IssueCountActive { get { return IssueCountOpen + IssueCountClosed; } }
+        public decimal IssueCloseRate { get { return Math.Round((decimal)IssueCountClosed / (decimal)IssueCountActive * 100m, 2); } }
+
+
+        public DashboardModel(IPtDashboardRepository rpsDashData)
+        {
+            rpsDashRepo = rpsDashData;
+        }
+
+        public void OnGet(int? userId, int? months)
+        {
+            ViewData.Add("userId", userId);
+            ViewData.Add("months", months);
+
+            DateTime start = months.HasValue ? DateTime.Now.AddMonths(months.Value * -1) : DateTime.Now.AddYears(-5);
+            DateTime end = DateTime.Now;
+
+            PtDashboardFilter filter = new PtDashboardFilter
+            {
+                DateStart = start,
+                DateEnd = end,
+                UserId = userId.HasValue ? userId.Value : 0
+            };
+
+            var statusCounts = rpsDashRepo.GetStatusCounts(filter);
+            IssueCountOpen = statusCounts.OpenItemsCount;
+            IssueCountClosed = statusCounts.ClosedItemsCount;
+
+            if (months.HasValue)
+            {
+                DateStart = filter.DateStart;
+                DateEnd = filter.DateEnd;
+            }
         }
     }
 }
