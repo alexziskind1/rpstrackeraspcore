d04b2beccc442e79256b8e46be16f50b6831b499
alexziskind1 <alex@nuvious.com>
2019-04-26 08:44:59 -0700

    added grid data model class

---
+++ RPS.Web/Models/PtItemGridModel.cs
@@ -0,0 +1,33 @@
+﻿using RPS.Core.Models;
+using System;
+
+namespace RPS.Web.Models
+{
+    public class PtItemGridModel
+    {
+        public int Id { get; private set; }
+        public string Title { get; private set; }
+        public DateTime DateCreated { get; private set; }
+
+        public int Estimate { get; private set; }
+        public string Priority { get; private set; }
+        public string Status { get; private set; }
+        public string Type { get; private set; }
+        public string AssigneeAvatar { get; private set; }
+        public string AssigneeFullName { get; private set; }
+
+        public PtItemGridModel(PtItem item)
+        {
+            Id = item.Id;
+            Title = item.Title;
+            DateCreated = item.DateCreated;
+
+            Estimate = item.Estimate;
+            Priority = item.Priority.ToString();
+            Status = item.Status.ToString();
+            Type = item.Type.ToString();
+            AssigneeAvatar = item.Assignee.Avatar;
+            AssigneeFullName = item.Assignee.FullName;
+        }
+    }
+}
